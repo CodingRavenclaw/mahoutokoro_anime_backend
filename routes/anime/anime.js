dd9b5ab796b88a62bd801f8eb5f4e344901965c3
ScienceRavenclaw <scienceravenclaw@rphg.com>
2022-03-28 23:05:31 +0200

    Added a route to get the total number of anime in the database.

--- routes/anime/anime.js
+++ routes/anime/anime.js
@@ -31,7 +31,7 @@ ROUTER.post('/anime/getanime',
     MAHOUTOKORO_MASTER_ACCESSOR.getConnection((err, con) => {
       if (err) throw err;
       let stmt = "SELECT anime.anime_id, anime.name AS 'anime_name', studio.name AS 'studio_name', author.first_name AS 'author_first_name', author.last_name AS 'author_last_name', " +
-        "anime.year, country.country_flag, anime.is_allowed " +
+        "anime.year, country.country_id, country.country_flag, anime.is_allowed " +
         "FROM anime, studio, author, country " +
         "WHERE (anime.studio_id = studio.studio_id AND anime.author_id = author.author_id AND anime.country = country.country_id) AND anime.name LIKE ?" +
         "ORDER BY " + con.escapeId(strOrderBy) + strOrderEscaped + " LIMIT ?, ?";
@@ -65,6 +65,26 @@ ROUTER.post('/anime/gettotalnumberofanime',
       con.query(stmt, arrInserts, (sqlErr, sqlRes, sqlFields) => {
         if (sqlErr) {
           console.error('/anime/gettotalnumberofanime: ' + sqlErr);
+          arrErrors.push(ERROR_HELPER('SERVER_ERROR'));
+          res.status(500).send({'errors': arrErrors});
+        } else {
+          res.status(200).send({'data': sqlRes});
+        }
+        con.release();
+        if (err) throw err;
+      });
+    });
+  });
+
+ROUTER.get('/anime/gettotalnumberofanimeindatabase',
+  (req, res) => {
+    arrErrors = [];
+    MAHOUTOKORO_MASTER_ACCESSOR.getConnection((err, con) => {
+      if (err) throw err;
+      let stmt = "SELECT COUNT(*) AS 'total_number_of_anime_in_database' FROM anime";
+      con.query(stmt, (sqlErr, sqlRes, sqlFields) => {
+        if (sqlErr) {
+          console.error('/anime/gettotalnumberofanimeindatabase: ' + sqlErr);
           arrErrors.push(ERROR_HELPER('SERVER_ERROR'));
           res.status(500).send({'errors': arrErrors});
         } else {
